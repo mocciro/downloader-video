a6fee4d0528cf4f317e6772b9fbdb39e551f4e7d
mocciro <kakang.arn07@gmail.com>
2025-12-23 20:06:57 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,23 +2,22 @@ import os
 from flask import Flask, render_template, request
 import yt_dlp
 
-# Konfigurasi folder templates agar terbaca dari dalam folder api
+# Seting folder template agar bisa terbaca dari folder api
 app = Flask(__name__, template_folder='../templates')
 
-# Tambahkan ini agar Vercel mengenali aplikasi sebagai 'app'
-application = app
+# Penting untuk Vercel
+app.debug = True
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     video_info = None
     if request.method == 'POST':
         url = request.form.get('url_video')
-      ydl_opts = {
+        # Konfigurasi agar ada suara dan pencarian cepat
+        ydl_opts = {
             'quiet': True,
             'no_warnings': True,
-            'check_formats': False,
-            # Tambahkan baris di bawah ini:
-            'format': 'best[ext=mp4]/best', 
+            'format': 'best[ext=mp4]/best',
         }
         try:
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
@@ -28,6 +27,5 @@ def home():
             
     return render_template('index.html', video=video_info)
 
-# Baris ini penting untuk local testing tapi tidak masalah jika ada di Vercel
-if __name__ == '__main__':
-    app.run(debug=True)
+# Ini variabel yang dicari Vercel
+app = app
